b007399dd8da4c66a4159856caecc2156ebb0008
b0risfosso <bjfosso@gmail.com>
2024-07-11 06:00:38 -0500

    print function in utils

--- utils.py
+++ utils.py
@@ -70,7 +70,7 @@ def train(neighborhood, nodes, list_neighbours, NUM_STEPS, NUM_SAMPLED, LEARNING
                 embeddings[u, :], embeddings[v_neg, :], gradientneg = update(embeddings[u, :], embeddings[v_neg, :], 0, LEARNING_RATE, nce_bias_neg)
         
         if k % (NUM_STEPS // 10) == 0:
-            print("Progress: {}/{} steps completed".format(k, NUM_STEPS))
+            print("Progress: " + str(k) + "/" + str(NUM_STEPS) + " steps completed")
 
     return embeddings
 
